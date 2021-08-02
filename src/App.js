1e154987e9083c5ec42517bea71f792439aad69a
m-a-hossain <adam.eve007@gmail.com>
2021-08-02 21:44:27 +0600

    start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h2>recux crud project</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
